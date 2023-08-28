c52392c704b05e1332d3d1f5067824b18151d3f7
Jamie <elibrij@gmail.com>
2023-08-28 15:16:54 -0400

    Rotors go turn (hopefully)

--- src/rotor.rs
+++ src/rotor.rs
@@ -1,8 +1,9 @@
+#[derive(Clone)]
 pub struct Rotor{
     forward_shifts: [i8; 26],
     inverse_shifts: [i8; 26],
-    turnover: char,
-    offset:i8,
+    turnover: usize,
+    turn_offset:usize,
 }
 impl Rotor{
 
@@ -13,24 +14,68 @@ impl Rotor{
         Rotor{
             forward_shifts: the_f_shifts,
             inverse_shifts: the_s_shifts,
-            turnover: the_turnover,
-            offset :0,
+            turnover: the_turnover as usize,
+            turn_offset: 0,
         }
 
     }
 
-    ///Turns rotor by incrementing offset.
-    /// resets offset to zero when 26 is reached
+    ///Turns rotor by incrementing turn_offset.
+    /// resets turn_offset to zero when 26 is reached
     pub fn turn(&mut self){
-        self.offset += 1;
-        if self.offset > 25{
-            self.offset = 0;
+        self.turn_offset += 1;
+        if self.turn_offset > 25{
+            self.turn_offset -= 26;
         }
     }
 
-    /// TODO implement, split into forward and inverse encrypt? 
-    /// Takes a character and returns it encoded
-    pub fn encode(&self, c: char) -> char{
-        c
+    ///encode a character on its first time through rotor
+    /// TODO pub(crate)
+    pub fn encode_forward(&self, c: &char) -> char{
+        let mut index: usize = *c as usize;
+        let mut new: i8 = index as i8;
+
+        index -= 97;
+        index += self.turn_offset;
+        if index > 25{
+            index -= 26;
+        }
+
+        new += self.forward_shifts[index];
+
+        if new > 122{
+            new -= 26;
+        }
+        else if new < 97{
+            new += 26
+        }
+
+        //println!("{}", index);
+        //println!("{}", new);
+        (new as u8) as char
+    }
+    ///encode a character on its second time through rotor
+    pub fn encode_inverse(&self, c: &char) -> char{
+        let mut index: usize = *c as usize;
+        let mut new: i8 = index as i8;
+
+        index -= 97;
+        index += self.turn_offset;
+        if index > 25{
+            index -= 26;
+        }
+
+        new += self.inverse_shifts[index];
+
+        if new > 122{
+            new -= 26;
+        }
+        else if new < 97{
+            new += 26;
+        }
+
+        //  println!("{}", index);
+        //println!("{}", new);
+        (new as u8) as char
     }
 }
